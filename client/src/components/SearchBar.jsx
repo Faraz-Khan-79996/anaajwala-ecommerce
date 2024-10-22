ba0b362dd4eb2b3ac07af3c0f53f9d067cd295b5
Faraz-Khan-79996 <farazgod1234@gmail.com>
2024-10-22 20:01:30 +0530

    optimised search

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,28 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 // import "./SearchBar.css";
 
-export default function SearchBar() {
+export default function SearchBar({products}) {
     const [input, setInput] = useState("");
     const [results, setResults] = useState([]);
     const [isFocused, setIsFocused] = useState(false);
   
-    const fetchData = (value) => {
-      fetch("/api/product/products")
-        .then((response) => response.json())
-        .then((json) => {
-          const filteredResults = json.filter((product) =>
-            value && product?.name?.toLowerCase().includes(value)
-          );
-          setResults(filteredResults);
-        });
-    };
+
+    // const fetchData = (value) => {
+    //   fetch("/api/product/products")
+    //     .then((response) => response.json())
+    //     .then((json) => {
+    //       const filteredResults = json.filter((product) =>
+    //         value && product?.name?.toLowerCase().includes(value)
+    //       );
+    //       setResults(filteredResults);
+    //     });
+    // };
   
     const handleChange = (value) => {
       setInput(value);
-      fetchData(value);
+      // fetchData(value);
+
+      const filteredResults = products.filter((product) => {
+        const productName = product?.name?.toLowerCase() || "";
+        
+        // Create a regex to match characters in order (fuzzy search)
+        const pattern = value
+          .toLowerCase()
+          .split("")
+          .map((char) => `.*${char}`)
+          .join(""); // Example: "ap" -> ".*a.*p"
+      
+        const regex = new RegExp(pattern);
+      
+        return regex.test(productName);
+      });
+      
+            
+      setResults(filteredResults);
     };
   
     const handleFocus = () => setIsFocused(true);
--- client/src/pages/products/Products.jsx
+++ client/src/pages/products/Products.jsx
@@ -39,7 +39,7 @@ function Products() {
 
     return (
         <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
-          <div className="pb-10"><SearchBar/></div>
+          <div className="pb-10"><SearchBar products={products}/></div>
             <div className="border-b mb-5 flex justify-between text-sm">
               <div className="text-indigo-600 flex items-center pb-2 pr-2 border-b-2 border-indigo-600 uppercase">
                 <svg
