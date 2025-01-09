40c06612ce59b0e9232c9e19c7839de956f14510
Faraz-Khan-79996 <farazgod1234@gmail.com>
2025-01-10 00:25:02 +0530

    changed the firebase configuration of mayur's

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,13 +7,15 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "anajwala-developement.firebaseapp.com",
-  projectId: "anajwala-developement",
-  storageBucket: "anajwala-developement.firebasestorage.app",
-  messagingSenderId: "845283237481",
-  appId: "1:845283237481:web:cf907354b478fcd4008cf8"
+  authDomain: "anajwala-99d67.firebaseapp.com",
+  projectId: "anajwala-99d67",
+  storageBucket: "anajwala-99d67.firebasestorage.app",
+  messagingSenderId: "232601522631",
+  appId: "1:232601522631:web:3a5beca45138fc2aca89e2",
+  measurementId: "G-H1DJVSFFLW"
 };
 
+// apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
